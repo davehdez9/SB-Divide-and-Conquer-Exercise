a4a10a2af7cbf9aae19d904de44d0e27cf50e27a
David <n.dave.hernandez@gmail.com>
2021-10-12 08:51:06 -0400

    sorted-frequency

--- prob-divide-conquer/sorted-frequency.js
+++ prob-divide-conquer/sorted-frequency.js
@@ -1,5 +1,38 @@
-function sortedFrequency() {
+function binarySearch( arr, value, search ){
+    let first = 0
+    let last = arr.length - 1
+    let result = -1
+    
+    
+    while( first <= last ){
+        let middle = Math.floor( ( first + last ) / 2 )
 
+        if( arr[middle] === value ){
+            result = middle
+            if( search ){
+                last = middle - 1
+            } else {
+             first = middle + 1
+            }
+        } else if ( value < arr[middle] ){
+            last = middle - 1
+        } else {
+         first = middle + 1
+        }
+    }
+    return result
+}
+
+
+function sortedFrequency( arr, value ) {
+    let firstIndex = binarySearch(arr, value, true)
+    let lastIndex = binarySearch(arr, value, false)
+
+    if( firstIndex === -1 ){
+        return -1
+    } else {
+        return lastIndex - firstIndex + 1
+    }
 }
 
 module.exports = sortedFrequency
